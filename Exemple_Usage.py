2e9599552103d82c9cb2a0118538a3580e71b5e9
Felipe Macedo Dias <fedias@usp.br>
2020-11-28 18:58:42 -0300

    Update and rename Zap_imoveis_Utilização.py to Exemple_Usage.py
    Example of usage of the functions.

--- Exemple_Usage.py
+++ Exemple_Usage.py
@@ -4,6 +4,12 @@ Created on Sat Nov 28 18:48:04 2020
 
 @author: femdi
 """
+'''
+AFTER RUNNING THE FUNCTIONS FROM 
+'''
+
+
+
 #importing necessary libs
 import pandas as pd
 import time
@@ -26,30 +32,34 @@ columns = ['Título','Endereço','Preço de Venda','Preço de Aluguel','Condomí
            'Creci','Características','Descrição','Latitude','Longitude']
 tabela_imoveis = pd.DataFrame(columns = columns)
 
-
-
 #Selection variables to be used in the URL
 localization="sp+sao-paulo+zona-sul+capao-redondo"
-num_pages=1
 acao = 'aluguel'
 tipo= 'imoveis'
+inclui_proximos = 'nao'
+bairros=["sp+sao-paulo+zona-sul+capao-redondo",'sp+sao-paulo+zona-sul+jd-sandra',
+         'sp+sao-paulo+zona-sul+vl-das-belezas']
 
-
-bairros=["sp+sao-paulo+zona-sul+capao-redondo",'sp+sao-paulo+zona-sul+jd-sandra']
-
-
+# For each neghborhood
 for loc in bairros:
     quant_local=24
-    while quant_local == 24:  
+    # While quant_local=24, the program will run and skip to the page (Because the previous page was full of houses)
+    while quant_local == 24: 
+        #opening the URL
         quant_local = abrir_zap(loc,acao,tipo)
         
-        if quant_local>0:
+        #If quant_local > 0, there is houses, so the prgram will run
+        if quant_local > 0:
             house_card = driver.find_elements_by_class_name('card-listing')[:quant_local]
+            
+            # Getting information for each card
             for i in range(len(house_card)):
                 
                 tabela_imoveis.loc[len(tabela_imoveis)] = extrair_informacoes(i)
-                
+            
+            #If this page is full of houses, turn to the next page to get the rest
             if quant_local == 24:
                 mudar_pagina()
+        
         
          
